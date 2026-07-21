b6a3ff80a2733124c61f85c9fb60ce1989dc86b6
gary <gary@WORK.local>
2026-07-21 11:31:03 +0200

    Init schema simplification

--- utils/sqldatabase/initschema.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-###################################################################################
-# This script is run from a utility Docker image of the Curity Identity Server.
-# It runs `idsvr -I` to trigger Liquibase to create or upgrade the database schema.
-###################################################################################
-
-idsvr -I
